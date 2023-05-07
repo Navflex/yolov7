32be152d8fcd94fef417972a616ff2de6e59e41c
Slava Tretyakov <slava@navflex.tech>
2023-05-07 22:03:34 +0200

    add tiny custom model configs

---
+++ my_custom_train_tiny.sh
@@ -0,0 +1,2 @@
+python train.py --workers 8 --device 0 --batch-size 32 --epochs 100 --data data/custom_data.yaml --img 640 640 --cfg cfg/training/yolov7-tiny_custom.yaml --weights weights/yolov7-tiny --name yolov7-tiny-custom --hyp data/hyp.scratch.tiny.yaml
+
