3606f542547ccbff5d5da16370296a8e427c9fb5
Slava Tretyakov <slava@navflex.tech>
2023-05-07 22:02:36 +0200

    change reparametrization to a tiny custom model

--- reparameterization.py
+++ reparameterization.py
@@ -7,11 +7,11 @@ import yaml
 
 device = select_device('0', batch_size=1)
 # model trained by cfg/training/*.yaml
-ckpt = torch.load('cfg/training/yolov7_training.pt', map_location=device)
+ckpt = torch.load('runs/train/yolov7-tiny-custom/weights/best.pt', map_location=device)
 # reparameterized model in cfg/deploy/*.yaml
-model = Model('cfg/deploy/yolov7.yaml', ch=3, nc=80).to(device)
+model = Model('cfg/deploy/yolov7-tiny_custom.yaml', ch=3, nc=5).to(device)
 
-with open('cfg/deploy/yolov7.yaml') as f:
+with open('cfg/deploy/yolov7-tiny_custom.yaml') as f:
     yml = yaml.load(f, Loader=yaml.SafeLoader)
 anchors = len(yml['anchors'][0]) // 2
 
@@ -25,15 +25,15 @@ model.nc = ckpt['model'].nc
 
 # reparametrized YOLOR
 for i in range((model.nc+5)*anchors):
-    model.state_dict()['model.105.m.0.weight'].data[i, :, :, :] *= state_dict['model.105.im.0.implicit'].data[:, i, : :].squeeze()
-    model.state_dict()['model.105.m.1.weight'].data[i, :, :, :] *= state_dict['model.105.im.1.implicit'].data[:, i, : :].squeeze()
-    model.state_dict()['model.105.m.2.weight'].data[i, :, :, :] *= state_dict['model.105.im.2.implicit'].data[:, i, : :].squeeze()
-model.state_dict()['model.105.m.0.bias'].data += state_dict['model.105.m.0.weight'].mul(state_dict['model.105.ia.0.implicit']).sum(1).squeeze()
-model.state_dict()['model.105.m.1.bias'].data += state_dict['model.105.m.1.weight'].mul(state_dict['model.105.ia.1.implicit']).sum(1).squeeze()
-model.state_dict()['model.105.m.2.bias'].data += state_dict['model.105.m.2.weight'].mul(state_dict['model.105.ia.2.implicit']).sum(1).squeeze()
-model.state_dict()['model.105.m.0.bias'].data *= state_dict['model.105.im.0.implicit'].data.squeeze()
-model.state_dict()['model.105.m.1.bias'].data *= state_dict['model.105.im.1.implicit'].data.squeeze()
-model.state_dict()['model.105.m.2.bias'].data *= state_dict['model.105.im.2.implicit'].data.squeeze()
+    model.state_dict()['model.77.m.0.weight'].data[i, :, :, :] *= state_dict['model.77.im.0.implicit'].data[:, i, : :].squeeze()
+    model.state_dict()['model.77.m.1.weight'].data[i, :, :, :] *= state_dict['model.77.im.1.implicit'].data[:, i, : :].squeeze()
+    model.state_dict()['model.77.m.2.weight'].data[i, :, :, :] *= state_dict['model.77.im.2.implicit'].data[:, i, : :].squeeze()
+model.state_dict()['model.77.m.0.bias'].data += state_dict['model.77.m.0.weight'].mul(state_dict['model.77.ia.0.implicit']).sum(1).squeeze()
+model.state_dict()['model.77.m.1.bias'].data += state_dict['model.77.m.1.weight'].mul(state_dict['model.77.ia.1.implicit']).sum(1).squeeze()
+model.state_dict()['model.77.m.2.bias'].data += state_dict['model.77.m.2.weight'].mul(state_dict['model.77.ia.2.implicit']).sum(1).squeeze()
+model.state_dict()['model.77.m.0.bias'].data *= state_dict['model.77.im.0.implicit'].data.squeeze()
+model.state_dict()['model.77.m.1.bias'].data *= state_dict['model.77.im.1.implicit'].data.squeeze()
+model.state_dict()['model.77.m.2.bias'].data *= state_dict['model.77.im.2.implicit'].data.squeeze()
 
 # model to be saved
 ckpt = {'model': deepcopy(model.module if is_parallel(model) else model).half(),
@@ -42,4 +42,4 @@ ckpt = {'model': deepcopy(model.module if is_parallel(model) else model).half(),
         'epoch': -1}
 
 # save reparameterized model
-torch.save(ckpt, 'cfg/deploy/yolov7.pt')
+torch.save(ckpt, 'runs/train/yolov7-tiny-custom/weights/yolov7-tiny_custom_deploy.pt')
