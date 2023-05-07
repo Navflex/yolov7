1b3c5da61fd5c08f61d9594605e0256b620ebf33
Slava Tretyakov <slava@navflex.tech>
2023-05-07 19:07:42 +0200

    add training configuration

---
+++ my_custom_train.sh
@@ -0,0 +1,2 @@
+python train.py --workers 8 --device 0 --batch-size 8 --epochs 100 --data data/custom_data.yaml --img 640 640 --cfg cfg/training/yolov7_custom.yaml --weights weights/yolov7_training.pt --name yolov7-custom --hyp data/hyp.scratch.custom.yaml
+
